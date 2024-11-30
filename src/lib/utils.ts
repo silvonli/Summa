bff97d29f1a4d85bd0df9bda0ccde1eca74873bc
silvon <silvonli@gmail.com>
2024-11-30 17:27:31 +0800

    Manual add shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 // 打印日志
 export function summaDebugLog(tag: string, ...messages: unknown[]) {
   if (process.env.NODE_ENV !== 'production') {
